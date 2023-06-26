4c27b712a5afb1f280a37cf31876c80da32e0ea2
Maedeh Yousefi <you.maede@gmail.com>
2023-06-26 19:22:22 +0430

    fix #24

---
+++ src/components/register/RegisterForm.vue
@@ -0,0 +1,230 @@
+<template>
+  <div class="registerForm-container">
+    <div class="top-blue-frame"></div>
+    <v-row>
+      <v-col cols="6" offset="3">
+        <v-form
+          @submit.prevent="onSubmit"
+          v-model="valid"
+          class="form elevation-15 rounded-xl">
+          <div class="title">ثبت نام در جی دامین</div>
+          <div>
+            برای ثبت نام، اطلاعات مورد نیاز زیر را وارد نمایید یا از طریق اکانت
+            های گوگل یا لینکدین و یا فیسبوک خود وارد شوید.
+          </div>
+          <div align="right" class="text-field-title">
+            ایمیل<span class="star-color">*</span>
+          </div>
+          <v-text-field
+            variant="outlined"
+            v-model="email"
+            :rules="emailRules"
+            required>
+            <template v-slot:append-inner>
+              <v-icon
+                color="#a2a2a2"
+                class="text-field-icon">mdi-email-outline
+              </v-icon>
+            </template>
+        </v-text-field>
+          <div align="right" class="text-field-title">
+            کلمه عبور<span class="star-color">*</span>
+          </div>
+          <v-text-field
+            variant="outlined"
+            v-model="password"
+            :rules="passwordRules"
+            required>
+            <template v-slot:append-inner>
+              <v-icon
+                color="#a2a2a2"
+                class="text-field-icon">mdi-lock-outline
+              </v-icon>
+            </template></v-text-field>
+          <div align="right" class="text-field-title">
+            تکرار کلمه عبور<span class="star-color">*</span>
+          </div>
+          <v-text-field
+            variant="outlined"
+            v-model="passwordConfirm"
+            :rules="passwordRules"
+            required>
+            <template v-slot:append-inner>
+              <v-icon
+                color="#a2a2a2"
+                class="text-field-icon">mdi-lock-outline
+              </v-icon>
+            </template></v-text-field>
+          <v-checkbox
+            v-model="Checkbox"
+            class="mt-2"
+            color="primary"
+            :rules="checkboxRules">
+            <template v-slot:label>همه ی <a class="link" href="#">قوانین و شراط</a>
+                 را خوانده وپذیرفته ام.
+            </template>
+          </v-checkbox>
+          <v-btn
+            type="submit"
+            :loading="loading"
+            variant="flat"
+            color="#47bd47"
+            rounded="pill"
+            width="80%"
+            height="45px"
+            class="btn-continue"
+            prepend-icon="mdi-arrow-left-thin">
+            ثبت نام
+          </v-btn>
+          <div>ورود با</div>
+          <v-btn
+            variant="outlined"
+            width="60px"
+            rounded="lg"
+            class="social-media-btn"
+            color="primary"
+            icon="x-larg mdi-facebook">
+          </v-btn>
+          <v-btn
+            variant="outlined"
+            width="60px"
+            rounded="lg"
+            class="social-media-btn"
+            color="primary"
+            icon="mdi-linkedin">
+          </v-btn>
+          <v-btn
+            variant="outlined"
+            width="60px"
+            rounded="lg"
+            class="social-media-btn"
+            color="primary"
+            icon="mdi-google">
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      valid: false,
+      email: "",
+      password: "",
+      passwordConfirm: "",
+      loading: false,
+      Checkbox: false,
+      emailRules: [
+        (value) => {
+          if (value) return true;
+          return "وارد کردن ایمیل الزامی است.";
+        },
+        (value) => {
+          if (
+            !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+              value
+            )
+          ) {
+            return "ایمیل وارد شده معتبر نمی باشد";
+          }
+          return true;
+        },
+      ],
+      passwordRules: [
+        (value) => {
+          if (value) return true;
+          return "وارد کردن رمز عبور الزامی است.";
+        },
+        (value) => {
+          if (value?.length >= 10) return true;
+          return "رمز عبور شما باید بیشتر از 10 کاراکتر باشد.";
+        },
+      ],
+      checkboxRules: [
+        (value) => {
+          if (value) return true;
+          return "برای ثبت نام در سایت باید قوانین را بپذیرید.";
+        },
+      ],
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.loading || !this.valid) {
+        return;
+      }
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+      }, 2000);
+    },
+  },
+});
+</script>
+<style lang="scss">
+.registerForm-container {
+  .top-blue-frame {
+    height: 120px;
+    background: #4f80ff;
+  }
+  .title {
+    font-size: 18px;
+    font-weight: 900;
+    margin-bottom: 20px;
+  }
+  .form {
+    background: white;
+    margin-top: -115px;
+    margin-bottom: 80px;
+    padding: 50px;
+    text-align: center;
+    .text-field-title {
+      color: #646464;
+      font-size: 13px;
+      margin-top: 17px;
+    }
+    .star-color {
+      color: #dd3f4e;
+    }
+    .link {
+      text-decoration: none;
+      margin: 1px 5px 0px 5px;
+      color: #4f80ff;
+    }
+    .v-field {
+      margin: 10px 0px 0px 0px;
+      --v-field-padding-top: 0px;
+      --v-input-padding-top: 0px;
+      --v-field-padding-bottom: 0px;
+      --v-input-control-height: 40px;
+      .text-field-icon{
+        border-right: 1px solid #a2a2a2;
+        height: 100%;
+        width: 30px;
+        padding-right: 8px;
+      }
+    }
+    .v-label {
+      --v-medium-emphasis-opacity: 1;
+      font-size: 13px;
+    }
+    .btn-continue {
+      font-family: IRANSans;
+      font-size: 16px;
+      font-weight: 700;
+      letter-spacing: 0;
+      color: white;
+      padding: 6px;
+      margin: 40px auto;
+    }
+  }
+  .social-media-btn{
+    margin: 20px 10px;
+    border:2px solid #ebebeb;
+  }
+}
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-locale-provider rtl>
+        <Navbar @clickOnNavBtn="navigation = true" />
+        <Navigation v-model="navigation" />
+        <GeneralHeader>
+        <template v-slot:title>
+            <p>ثبت نام</p>
+        </template>
+        <template v-slot:content>
+            <p>صفحه اصلی > ثبت نام</p>
+        </template>
+        </GeneralHeader>
+        <RegisterForm />
+        <GeneralFooter />
+    </v-locale-provider>
+</template>
+<script lang="ts">
+import Navbar from "@/components/Navbar.vue";
+import GeneralHeader from "@/components/GeneralHeader.vue";
+import GeneralFooter from "@/components/GeneralFooter.vue";
+import RegisterForm from "@/components/register/RegisterForm.vue";
+
+import { defineComponent } from "vue";
+import { load } from "webfontloader";
+
+export default defineComponent({
+  components: {
+    Navbar,
+    GeneralHeader,
+    RegisterForm,
+    GeneralFooter,
+  },
+  data() {
+    return {
+      navigation: false,
+    };
+  },
+})
+</script>
